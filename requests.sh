221f6c8fde6fd94874e4e449dc1b192594d82a0a
Arno Burnuk <arno.burnuk@gmail.com>
2024-08-27 01:36:41 +0000

    updated requests.sh

--- requests.sh
+++ requests.sh
@@ -1,9 +1,7 @@
-curl -v POST https://super-spork-69grwrw9p4724wg5-3000.app.github.dev/api/submit \
-     -H "Content-Type: application/json" \
-     -d '{"name": "John Doe", "email": "john@example.com"}'
+curl -X POST localhost:3000/api/incidents -H "Content-Type: application/json" -d @data.json
+
+curl https://super-spork-69grwrw9p4724wg5-3000.app.github.dev/api/incidents
 
 curl -i https://super-spork-69grwrw9p4724wg5-3000.app.github.dev/api/posts
 
 curl -i https://super-spork-69grwrw9p4724wg5-3000.app.github.dev/api/hello
-
- curl 148.113.138.137:27017
