e2dba1eb95da2dc797bd3762d6ffd433dad4f335
Arno Burnuk <arno.burnuk@gmail.com>
2024-09-01 17:41:50 +0000

    added query by userId

---
+++ pages/api/incidents/[userId].js
@@ -0,0 +1,25 @@
+import clientPromise from '../../../lib/mongodb';
+
+export default async function handler(req, res) {
+    try {
+        const client = await clientPromise;
+        const db = client.db(process.env.MONGODB_DB);
+
+        const { userId } = req.query;
+
+        if (req.method === 'GET') {
+            // Handle GET request - fetch incidents by userId
+            const incidents = await db.collection('incidents').find({ 'assigned_to.id': userId }).toArray();
+            console.log(incidents);
+
+            res.status(200).json(incidents);
+        } else {
+            // Handle unsupported methods
+            res.setHeader('Allow', ['GET']);
+            res.status(405).end(`Method ${req.method} Not Allowed`);
+        }
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: 'Unable to process request' });
+    }
+}
--- requests.sh
+++ requests.sh
@@ -1,6 +1,8 @@
 curl -X POST localhost:3000/api/incidents -H "Content-Type: application/json" -d @data.json
 
-curl https://super-spork-69grwrw9p4724wg5-3000.app.github.dev/api/incidents
+curl localhost:3000/api/incidents
+
+curl localhost:3000/api/incidents/PLM1TEA
 
 curl -i https://super-spork-69grwrw9p4724wg5-3000.app.github.dev/api/posts
 
